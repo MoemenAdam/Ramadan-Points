dfc324c47372444d6263c71eb82b7db82a388e1d
Moemen Adam <mmuummnna@gmail.com>
2024-03-04 02:22:12 +0200

    add Home and Signup Components folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router"
 import { AnimatePresence } from "framer-motion"
 
+import Home from "./Pages/Home"
+import NotFound from "./Pages/NotFound"
+
+
 function App() {
   return (
     <AnimatePresence>
       <Routes>
-        <Route />
-        <Route />
+        <Route path="/" element={<Home />}/>
+        <Route path="/*" element={<NotFound />}/>
       </Routes>
     </AnimatePresence>
   )
