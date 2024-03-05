e307f7850baf8f11a95c0e376c52be32b78092c9
Moemen Adam <mmuummnna@gmail.com>
2024-03-05 23:39:55 +0200

    add audio auto play and quran api

--- src/components/HomeComponents/QuranComponents/Quran.jsx
+++ src/components/HomeComponents/QuranComponents/Quran.jsx
@@ -1,9 +1,109 @@
-export default function Quran() {
+import { useEffect, useState } from "react";
+import { useFetch } from "../../../CustomHooks/useFetch"
+import SurahLoader from "./SurahLoader"
+
+const init = {
+  "number": 1,
+  "name": "سُورَةُ ٱلْفَاتِحَةِ",
+  "englishName": "Al-Faatiha",
+  "englishNameTranslation": "The Opening",
+  "revelationType": "Meccan",
+  "ayahs": [
+      {
+          "number": 1,
+          "text": "﻿بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ",
+          "numberInSurah": 1,
+          "juz": 1,
+          "manzil": 1,
+          "page": 1,
+          "ruku": 1,
+          "hizbQuarter": 1,
+          "sajda": false
+      },
+      {
+          "number": 2,
+          "text": "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ",
+          "numberInSurah": 2,
+          "juz": 1,
+          "manzil": 1,
+          "page": 1,
+          "ruku": 1,
+          "hizbQuarter": 1,
+          "sajda": false
+      },
+      {
+          "number": 3,
+          "text": "الرَّحْمَٰنِ الرَّحِيمِ",
+          "numberInSurah": 3,
+          "juz": 1,
+          "manzil": 1,
+          "page": 1,
+          "ruku": 1,
+          "hizbQuarter": 1,
+          "sajda": false
+      },
+      {
+          "number": 4,
+          "text": "مَالِكِ يَوْمِ الدِّينِ",
+          "numberInSurah": 4,
+          "juz": 1,
+          "manzil": 1,
+          "page": 1,
+          "ruku": 1,
+          "hizbQuarter": 1,
+          "sajda": false
+      },
+      {
+          "number": 5,
+          "text": "إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ",
+          "numberInSurah": 5,
+          "juz": 1,
+          "manzil": 1,
+          "page": 1,
+          "ruku": 1,
+          "hizbQuarter": 1,
+          "sajda": false
+      },
+      {
+          "number": 6,
+          "text": "اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ",
+          "numberInSurah": 6,
+          "juz": 1,
+          "manzil": 1,
+          "page": 1,
+          "ruku": 1,
+          "hizbQuarter": 1,
+          "sajda": false
+      },
+      {
+          "number": 7,
+          "text": "صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ",
+          "numberInSurah": 7,
+          "juz": 1,
+          "manzil": 1,
+          "page": 1,
+          "ruku": 1,
+          "hizbQuarter": 1,
+          "sajda": false
+      }
+  ]
+}
+
+export default function Quran({surahNumber, setSurahNumber}) {
+  const {data: surahsData, loading: surahsLoading} = useFetch('https://api.alquran.cloud/v1/quran/ar.asad')
+  const [surah,setSurah] = useState(init);
+  useEffect(()=>{
+    setSurah(surahsData?.data?.surahs?.filter(e=>e.number === surahNumber)[0] || init);
+  },[surahNumber])
+
+
   // cooment for future
   // qframe has 3 parts top, middle, bottom
   // qframeMiddle has quranText 
   // quranText has suraHeaderFrame if exist and ayaText
   // quranText has semi yellow background
+
+
 
   // now how to use
   // qFrameTop for top
@@ -13,22 +113,43 @@ export default function Quran() {
   // ayaText for aya text
   // quranText for quran text
   return (
-    <div className='w-[500px]'>
-      <div className="qFrame qFrameTop"></div>
+    <>
+      {surahsLoading ?
+        <SurahLoader />
 
-      <div className="qFrameMiddle ">
-        <div className="quranText" id="quranText">
-          <div className="suraHeaderFrame">سورة الأنعام</div>
-          <div className="ayaText text-center">بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ </div>
-          <div className="leading-[55px]">
+        :
+      
+        <div className='w-[500px]'>
+          <div className="qFrame qFrameTop"></div>
 
-            هُوَ الَّذِي خَلَقَكُم مِّن طِينٍ ثُمَّ قَضَىٰ أَجَلًا ۖ وَأَجَلٌ مُّسَمًّى عِندَهُ ۖ ثُمَّ أَنتُمْ تَمْتَرُونَ &lrm;﴿٢﴾&lrm; وَهُوَ اللَّهُ فِي السَّمَاوَاتِ وَفِي الْأَرْضِ ۖ يَعْلَمُ سِرَّكُمْ وَجَهْرَكُمْ وَيَعْلَمُ مَا تَكْسِبُونَ ‎﴿٣﴾‏ فَقَدْ كَذَّبُوا بِالْحَقِّ لَمَّا جَاءَهُمْ ۖ فَسَوْفَ يَأْتِيهِمْ أَنبَاءُ مَا كَانُوا بِهِ يَسْتَهْزِئُونَ ‎﴿٥﴾‏ أَلَمْ يَرَوْا كَمْ أَهْلَكْنَا مِن قَبْلِهِم مِّن قَرْنٍ مَّكَّنَّاهُمْ فِي الْأَرْضِ مَا لَمْ نُمَكِّن لَّكُمْ وَأَرْسَلْنَا السَّمَاءَ عَلَيْهِم مِّدْرَارًا وَجَعَلْنَا الْأَنْهَارَ تَجْرِي مِن تَحْتِهِمْ فَأَهْلَكْنَاهُم بِذُنُوبِهِمْ وَأَنشَأْنَا مِن بَعْدِهِمْ قَرْنًا آخَرِينَ ‎﴿٦﴾‏ وَلَوْ نَزَّلْنَا عَلَيْكَ كِتَابًا فِي قِرْطَاسٍ فَلَمَسُوهُ بِأَيْدِيهِمْ لَقَالَ الَّذِينَ كَفَرُوا إِنْ هَٰذَا إِلَّا سِحْرٌ مُّبِينٌ ‎﴿٧﴾‏ وَقَالُوا لَوْلَا أُنزِلَ عَلَيْهِ مَلَكٌ ۖ وَلَوْ أَنزَلْنَا مَلَكًا لَّقُضِيَ الْأَمْرُ ثُمَّ لَا يُنظَرُونَ ‎﴿٨﴾‏
-          </div>
+          {/* <div className="qFrameMiddle ">
+            <div className="quranText" id="quranText">
+              <div className="suraHeaderFrame">سورة الأنعام</div>
+              <div className="ayaText text-center">بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ </div>
+              <div className="leading-[55px]">
 
+                هُوَ الَّذِي خَلَقَكُم مِّن طِينٍ ثُمَّ قَضَىٰ أَجَلًا ۖ وَأَجَلٌ مُّسَمًّى عِندَهُ ۖ ثُمَّ أَنتُمْ تَمْتَرُونَ &lrm;﴿٢﴾&lrm; وَهُوَ اللَّهُ فِي السَّمَاوَاتِ وَفِي الْأَرْضِ ۖ يَعْلَمُ سِرَّكُمْ وَجَهْرَكُمْ وَيَعْلَمُ مَا تَكْسِبُونَ ‎﴿٣﴾‏ فَقَدْ كَذَّبُوا بِالْحَقِّ لَمَّا جَاءَهُمْ ۖ فَسَوْفَ يَأْتِيهِمْ أَنبَاءُ مَا كَانُوا بِهِ يَسْتَهْزِئُونَ ‎﴿٥﴾‏ أَلَمْ يَرَوْا كَمْ أَهْلَكْنَا مِن قَبْلِهِم مِّن قَرْنٍ مَّكَّنَّاهُمْ فِي الْأَرْضِ مَا لَمْ نُمَكِّن لَّكُمْ وَأَرْسَلْنَا السَّمَاءَ عَلَيْهِم مِّدْرَارًا وَجَعَلْنَا الْأَنْهَارَ تَجْرِي مِن تَحْتِهِمْ فَأَهْلَكْنَاهُم بِذُنُوبِهِمْ وَأَنشَأْنَا مِن بَعْدِهِمْ قَرْنًا آخَرِينَ ‎﴿٦﴾‏ وَلَوْ نَزَّلْنَا عَلَيْكَ كِتَابًا فِي قِرْطَاسٍ فَلَمَسُوهُ بِأَيْدِيهِمْ لَقَالَ الَّذِينَ كَفَرُوا إِنْ هَٰذَا إِلَّا سِحْرٌ مُّبِينٌ ‎﴿٧﴾‏ وَقَالُوا لَوْلَا أُنزِلَ عَلَيْهِ مَلَكٌ ۖ وَلَوْ أَنزَلْنَا مَلَكًا لَّقُضِيَ الْأَمْرُ ثُمَّ لَا يُنظَرُونَ ‎﴿٨﴾‏
+              </div>
+
+            </div>
+          </div> */}
+            <div className="qFrameMiddle ">
+              <div className="quranText" id="quranText">
+                <div className="suraHeaderFrame">{surah.name}</div>
+                <div className="flex flex-wrap">
+                    {surah?.ayahs?.map((aya) => (
+                      <div key={aya.number}>
+                        <div className="ayaText text-center">{aya.text}</div>
+                      </div>
+                      ))
+                    }
+                </div>
+              </div>
+            </div>
+
+          <div className="qFrame qFrameBottom"></div>
         </div>
-      </div>
-
-      <div className="qFrame qFrameBottom"></div>
-    </div>
+      }
+    </>
   )
 }
--- src/components/HomeComponents/QuranComponents/QuranSearch.jsx
+++ src/components/HomeComponents/QuranComponents/QuranSearch.jsx
@@ -1,24 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import SurahLoader from "./SurahLoader"
 
 import { useFetch } from '../../../CustomHooks/useFetch'
 
-
-const SurahLoader = () => {
-  return (
-    <div className='flex justify-center items-center flex-grow'>
-      <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin  fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-      </svg>
-    </div>
-  )
-}
-
-export default function QuranSearch() {
+export default function QuranSearch({surahNumber, setSurahNumber}) {
   const [type , setType] = useState('surah')
-  const [surahNumber, setSurahNumber] = useState(1)
   const [ReaderName, setReaderName] = useState('مشاري العفاسي')
+  const [autoPlay, setAutoPlay] = useState(false);
   const {data: surahsData, loading: surahsLoading} = useFetch('https://api.alquran.cloud/v1/surah')
   const {data: readers, loading: readersLoading} = useFetch('https://mp3quran.net/api/v3/reciters')
   
@@ -71,7 +60,7 @@ export default function QuranSearch() {
                         surahsData.data.map(e=>{
                         return(
                           <motion.div
-                          onClick={()=>{handleSurahChoose(e.number)}} key={e.number} 
+                          onClick={()=>{setAutoPlay(false);handleSurahChoose(e.number)}} key={e.number} 
                           className={`flex justify-between py-3 mb-4 mr-2 pr-3 pl-3  cursor-pointer select-none gap-5 ${surahNumber===e.number?'bg-blue-300 transition-colors duration-500':''}`}>
                             <h1 className='w-fit'> {e.name.split('سُورَةُ').join(' ')} </h1>
                             <h1 className='text-left w-fit'> {e.englishName} </h1>
@@ -117,7 +106,6 @@ export default function QuranSearch() {
               <select onClick={handleReaderChoose} className='w-[80%] outline-none select-dropdown rounded-3xl' name="Reader" id="">
                 <option value="مشاري العفاسي"> مشاري العفاسي  </option>
                 <option value="علي جابر"> علي جابر </option>
-                <option value="بندر بليلة"> بندر بليلة </option>
                 <option value="ماهر المعيقلي"> ماهر المعيقلي </option>
                 <option value="ياسر الدوسري"> ياسر الدوسري </option>
                 <option value="عبدالله عواد الجهني"> عبدالله عواد الجهني </option>
@@ -125,7 +113,9 @@ export default function QuranSearch() {
               </select>
             </div>
             <div className='mt-2'>
-              <audio className='outline-none' controls src={surahUrl}/>
+              {autoPlay && <audio className='outline-none' autoPlay onEnded={()=>{setAutoPlay(true) ;setSurahNumber(prev=>(prev+1)%114)}} controls src={surahUrl}/>}
+
+              {!autoPlay && <audio className='outline-none' onEnded={()=>{setAutoPlay(true) ;setSurahNumber(prev=>(prev+1)%114)}} controls src={surahUrl}/>}
             </div>
           </div>
         }
