20ac6b4b503d141228ec08ae1a9869ec3bca97f6
Moemen Adam <mmuummnna@gmail.com>
2024-03-10 00:42:57 +0200

    add BackgroundStore storage

---
+++ src/store/BackgroundStore.js
@@ -0,0 +1,51 @@
+export const welcomePage ={
+  backgroundImage: `url(/assets/background.jpg)`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+}
+
+export const mainPage ={
+  backgroundImage: `url(/assets/texture.svg)`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+}
+
+export const surahbg ={
+  backgroundImage: `url(/assets/surahbg2.png)`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100% 100%',
+}
+
+
+export const loginbg ={
+  backgroundImage: `url(/assets/Loginbg.png)`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100% 100%',
+}
+
+export const duasTop ={
+  backgroundImage: `url(/assets/duas1.png)`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+}
+
+export const duasLeft ={
+  backgroundImage: `url(/assets/duas2.png)`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100% 100%',
+}
+
+
+export const Fanoos ={
+  backgroundImage: `url(/assets/Lantern.svg)`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+}
+
