a4e8737397734f762bdb6ba3ec51725c8936f8c0
Mustafa Elsharawy <memomeme621@gmail.com>
2024-03-10 16:46:59 +0200

    fixing some bugs in leaderboard

--- src/components/LeaderBoardComponents/TopUsers.jsx
+++ src/components/LeaderBoardComponents/TopUsers.jsx
@@ -6,7 +6,7 @@ export default function TopUsers({data,currentUser}) {
   return (
     <div className="flex nav:flex-row flex-col gap-5 gap-y-32 nav:mt-60 mt-32 h-full">
       <div className={`relative ${data.third.id!==currentUser?'Topbg':'TopbgCurrent'} w-[300px] h-[230px] nav:order-1 order-3 flex justify-center items-center`}>
-        <img style={{boxShadow:'0px 14px 100px 0px #C47A31'}} className='w-24 rounded-2xl absolute -top-32' src="assets/sambosa1.jpg" alt="" />
+        {data.third.id && <img style={{boxShadow:'0px 14px 100px 0px #C47A31'}} className='w-24 rounded-2xl absolute -top-32' src="assets/sambosa1.jpg" alt="" />}
         <p className='absolute -top-6 text-white font-medium text-lg'>{data.third.name}</p>
         
         <div className='flex flex-col-reverse gap-2 text-center text-white'>
@@ -18,7 +18,7 @@ export default function TopUsers({data,currentUser}) {
         </div>
       </div>
       <div className={`relative ${data.first.id!==currentUser?'Topbg':'TopbgCurrent'} w-[300px] h-[230px] nav:order-1 order-1 nav:-top-20 flex justify-center items-center`}>
-        <img style={{boxShadow:'0px 14px 100px 0px #F6CB01'}} className='w-24 rounded-2xl absolute -top-32' src="assets/gozhend1.png" alt="" />
+      {data.first.id && <img style={{boxShadow:'0px 14px 100px 0px #F6CB01'}} className='w-24 rounded-2xl absolute -top-32' src="assets/gozhend1.png" alt="" />}
         <p className='absolute -top-6 text-white font-medium text-lg'>{data.first.name}</p>
         
         <div className='flex flex-col-reverse gap-2 text-center text-white'>
@@ -31,7 +31,7 @@ export default function TopUsers({data,currentUser}) {
 
       </div>
       <div className={`relative ${data.second.id!==currentUser?'Topbg':'TopbgCurrent'} w-[300px] h-[230px] nav:order-1 order-2 flex justify-center items-center`}>
-        <img style={{boxShadow:'0px 14px 100px 0px #B6B5B5'}} className='w-24 rounded-2xl absolute -top-32' src="assets/sambosa2.png" alt="" />
+      {data.second.id && <img style={{boxShadow:'0px 14px 100px 0px #B6B5B5'}} className='w-24 rounded-2xl absolute -top-32' src="assets/sambosa2.png" alt="" />}
         <p className='absolute -top-6 text-white font-medium text-lg'>{data.second.name}</p>
         
         <div className='flex flex-col-reverse gap-2 text-center text-white'>
